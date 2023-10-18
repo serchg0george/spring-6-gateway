612ea0faafccf8a55eb9aa09fdc9c3d210c6a068
serchg0george <serchg0george@gmail.com>
2023-10-18 16:24:13 +0300

    Implemented resource-server logic

---
+++ src/main/java/com/springframeork/spring6gateway/config/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.springframeork.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authExchange -> authExchange.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth -> oAuth.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
