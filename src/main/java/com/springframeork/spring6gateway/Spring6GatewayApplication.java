9e4b68e7a4bdd579a4e2e8a4b7af837b38457789
serchg0george <serchg0george@gmail.com>
2023-10-18 15:16:34 +0300

    Initial commit

---
+++ src/main/java/com/springframeork/spring6gateway/Spring6GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springframeork.spring6gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6GatewayApplication.class, args);
+    }
+
+}
